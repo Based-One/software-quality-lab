f35521b8bed509ae6cefbbc86c069b55650f84d1
Work <aesteban@cetys.edu.mx>
2026-03-03 20:29:46 -0800

    Develop tests for @EmailValidatorService achieving 91% of branch coverage using JaCoCo

--- src/main/java/mx/edu/cetys/software_quality_lab/EmailValidatorService.java
+++ src/main/java/mx/edu/cetys/software_quality_lab/EmailValidatorService.java
@@ -2,12 +2,55 @@ package mx.edu.cetys.software_quality_lab;
 
 public class EmailValidatorService {
 
-    public boolean isValid(String email) {
-        if (email == null || email.isEmpty()) {
-            return false;
+    private static final String[] DIPTONGOS = {
+            "ai", "au", "ei", "ey", "ie", "oi", "ou", "oy"
+    };
+
+    public boolean isValid(String inputEmail) {
+        if (inputEmail == null) return false;
+
+        String email = inputEmail.toLowerCase();
+
+        return isEmpty(email)
+                && hasValidFormat(email)
+                && doesNotContainDiphthongs(email)
+                && hasValidLength(email)
+                && doesContainNumberFour(email);
+    }
+
+    public boolean isEmpty(String email) {
+        return !email.isEmpty();
+    }
+
+    public boolean hasValidFormat(String inputEmail) {
+        String email = inputEmail.toLowerCase();
+
+        return email.matches("^[a-z0-9]{1,10}#[a-z]{1,20}\\.[a-z]{1,5}$");
+    }
+
+    public boolean doesNotContainDiphthongs(String email) {
+        for (String dip : DIPTONGOS) {
+            if (email.contains(dip)) {
+                return false;
+            }
         }
         return true;
     }
+    public boolean doesContainNumberFour(String email){
+        for (int i = 0; i < email.length(); i++) {
+           if(email.charAt(i) == '4'){
+               return true;
+           }
+        }
+        return false;
+    }
+
+    public boolean hasValidLength(String inputEmail) {
+        String email = inputEmail.toLowerCase();
+
+        return email.length() < 47;
+    }
+
 
 
 }
--- src/test/java/mx/edu/cetys/software_quality_lab/EmailValidatorServiceTest.java
+++ src/test/java/mx/edu/cetys/software_quality_lab/EmailValidatorServiceTest.java
@@ -1,25 +1,75 @@
 package mx.edu.cetys.software_quality_lab;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class EmailValidatorServiceTest {
+
+    EmailValidatorService emailValidator;
+
+    @BeforeEach
+    void setUp(){
+        emailValidator = new EmailValidatorService();
+    }
+
     @Test
-    void shouldReturnFalse_WhenEmailIsNull()
+    void shouldReturnFalse_WhenEmailIsNull(){
+        assertFalse(emailValidator.isValid(null));
+    }
 
-    {
-        //Arrange
-        EmailValidatorService emailValidator = new EmailValidatorService();
-        var isValid = emailValidator.isValid(null);
-        assertFalse(isValid);
+    @Test
+    void shouldReturnFalse_WhenEmailIsEmpty(){
+        assertFalse(emailValidator.isValid(""));
+    }
 
+    @Test
+    void shouldReturnFalse_WhenCharactersAreInvalid(){
+        assertFalse(emailValidator.isValid("user!#domain.com"));
+        assertFalse(emailValidator.isValid("user #domain.com"));
+        assertFalse(emailValidator.isValid("user$#domain.com+"));
+    }
+
+    @Test
+    void shouldReturnFalse_WhenThereIsAnArrobaInsteadOfHashtag(){
+        assertFalse(emailValidator.isValid("user4@gmil.com"));
+    }
+
+    @Test
+    void shouldReturnFalse_WhenThereIsDiphthong(){
+        assertFalse(emailValidator.isValid("aur4n#gmail.com"));
     }
     @Test
-    void shouldReturnFalse_WhenEmailIsEmpty()
-    {
-        EmailValidatorService emailValidator = new EmailValidatorService();
-        var isValid = emailValidator.isValid("");
-        assertFalse(isValid);
+    void shouldReturnTrue_WhenThereIsNotDiphthongs(){
+        assertTrue(emailValidator.isValid("brt4#xyz.com"));
     }
+    @Test
+    void shouldReturnFalse_WhenThereIsNoNumberFour(){
+        assertFalse(emailValidator.isValid("user#gmil.com"));
+    }
+
+    @Test
+    void shouldReturnFalse_WhenEmailIsTooLong(){
+        String longEmail = "aaaaaaaaaa4#bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb.com";
+        assertFalse(emailValidator.isValid(longEmail));
+    }
+
+    @Test
+    void shouldReturnTrue_WhenEmailIsCompletelyValid(){
+        assertTrue(emailValidator.isValid("user4#gmil.abc"));
+    }
+
+    @Test
+    void shouldReturnFalse_WhenEmailIsCompletelyInvalid(){
+        assertFalse(emailValidator.isValid("asasdfdasfdasfdasfdasfdsafdasadsfasdfdasdf@gmail.comasdf"));
+    }
+
+
+
+
+
+
+
+
 }
